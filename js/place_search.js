ee4611db6aeeccd4a152e2e9f21cfca11ae34173
[SW2/서울/2반] 한진희 <gksgml13@gmail.com>
2022-04-29 12:11:51 +0900

    1211

--- js/place_search.js
+++ js/place_search.js
@@ -20,6 +20,9 @@ var ps = new kakao.maps.services.Places();
 // 키워드로 장소를 검색합니다
 ps.keywordSearch(search, placesSearchCB); 
 
+var wantLength= 3;
+
+
 // 키워드 검색 완료 시 호출되는 콜백함수 입니다
 function placesSearchCB (data, status, pagination) {
     if (status === kakao.maps.services.Status.OK) {
@@ -28,7 +31,8 @@ function placesSearchCB (data, status, pagination) {
         // LatLngBounds 객체에 좌표를 추가합니다
         var bounds = new kakao.maps.LatLngBounds();
 
-        for (var i=0; i<data.length; i++) {
+        // for (var i=0; i<data.length; i++) {
+        for (var i=0; i<wantLength; i++) {
             displayMarker(data[i]);    
             bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
         }       
