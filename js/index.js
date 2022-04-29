eea2d5c7ce9d1faf006380af203288148e667429
[SW2/서울/2반] 한진희 <gksgml13@gmail.com>
2022-04-29 16:30:00 +0900

    1630

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 const API_KEY="d2c902d6ac6326cbb6156dacfebcb420"
 
 function onGeoOk(position){
-    global lat = position.coords.latitude;
-    global lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
     fetch(url).then(response => response.json()).then(data => {
         
         const city = document.querySelector("#location p:first-Child")
@@ -12,8 +12,6 @@ function onGeoOk(position){
         city.innerText = `현재 위치: ${data.name}`;
         weather.innerText = `현재 날씨: ${data.weather[0].main} / 현재 온도: ${data.main.temp}`;
         location.innerText= `위도: ${lat}  경도: ${lon}`
-        var aaa=1;
-        bbb=2;
         });
 }
 
@@ -37,8 +35,7 @@ map.setCenter(moveLatLon);
 
 
 var markerPosition  = new kakao.maps.LatLng(lat, lon);
-var aaa=1
-bbb=2
+
 // 마커를 생성합니다
 var marker = new kakao.maps.Marker({
     position: markerPosition
