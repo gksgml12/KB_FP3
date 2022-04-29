9435757ba9ff8c85d8486be6a48195b712dd4ded
[SW2/서울/2반] 한진희 <gksgml13@gmail.com>
2022-04-29 16:43:42 +0900

    1640

--- js/index.js
+++ js/index.js
@@ -3,9 +3,8 @@ const API_KEY="d2c902d6ac6326cbb6156dacfebcb420"
 function onGeoOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
     fetch(url).then(response => response.json()).then(data => {
-        
         const city = document.querySelector("#location p:first-Child")
         const weather = document.querySelector("#location p:nth-Child(2)")
         const location = document.querySelector("#location p:nth-Child(3)")
